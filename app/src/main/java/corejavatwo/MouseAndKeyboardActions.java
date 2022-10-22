bbdcdcc0c90bb7fb525fd06f716883b0b7e36327
Pratik <impratikbhowmik@gmail.com>
2022-10-22 14:04:53 +0530

    Mouse and keyboard

--- app/src/main/java/corejavatwo/MouseAndKeyboardActions.java
+++ app/src/main/java/corejavatwo/MouseAndKeyboardActions.java
@@ -1,10 +1,7 @@
 package corejavatwo;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
@@ -19,22 +16,60 @@ public class MouseAndKeyboardActions {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("window.scrollBy(0,1500)");
 
-        WebElement clickElement = driver.findElement(By.xpath("//a[@href='/Mouse']"));
+        WebElement clickElement = driver.findElement(By.xpath("//a[@href='/Auto-complete']"));
         Actions act = new Actions(driver);
         act.click(clickElement).perform();
 
-        js.executeScript("window.scrollBy(0,1300)");
 
-        //WebElement hoverElement = driver.findElement(By.xpath("//div[@class = 'Mouse_section4']/p[text() = 'Password']"));
-        //act.moveToElement(hoverElement).perform();
+        WebElement autoComplete = driver.findElement(By.xpath("//input"));
 
-        WebElement dragmearound = driver.findElement(By.xpath("//div[text() = 'Drag me around']"));
-        act.dragAndDropBy(dragmearound, 100, 100);
+        act.click(autoComplete).sendKeys("Art").perform();
 
-        WebElement dragAndDropElement = driver.findElement(By.xpath("//div[text() = 'Drag me to target']"));
-        WebElement destination = driver.findElement(By.xpath("//div[@class = 'box drop-target react-draggable']"));
+//        js.executeScript("window.scrollBy(0,900)");
 
-        act.dragAndDrop(dragAndDropElement, destination).build().perform();
-        Thread.sleep(3000);
+//        WebElement namefield = driver.findElement(By.xpath("//input[@placeholder = 'Name of  the user']"));
+//
+//        namefield.sendKeys("Pratik");
+//
+//        act.sendKeys(Keys.TAB);
+//
+//        WebElement emailField = driver.findElement(By.xpath("//input[@placeholder = 'example@example.com']"));
+//
+//        emailField.sendKeys("impratikbhowmik@gmail.com");
+//
+//        act.sendKeys(Keys.TAB);
+//
+//        WebElement phoneNumber = driver.findElement(By.xpath("//input[@type = 'number']"));
+//
+//        phoneNumber.sendKeys("123456");
+//
+//        phoneNumber.sendKeys(Keys.TAB);
+//
+//        WebElement copyPasteElement = driver.findElement(By.xpath("//div[text() = 'Copy me and paste me in the below box']"));
+//
+//        act.click(copyPasteElement).perform();
+//
+//        act.keyDown(Keys.CONTROL).sendKeys("c").keyUp(Keys.CONTROL).build().perform();
+//
+//        WebElement pasteElement = driver.findElement(By.xpath("(//input[@type = 'text'])[2]"));
+//
+//        act.click(pasteElement).perform();
+//
+//        act.keyDown(Keys.CONTROL).sendKeys("v").keyUp(Keys.CONTROL).build().perform();
+
+
+
+
+
+//        WebElement hoverElement = driver.findElement(By.xpath("//div[@class = 'Mouse_section4']/p[text() = 'Password']"));
+//        act.moveToElement(hoverElement).perform();
+//
+//        WebElement dragmearound = driver.findElement(By.xpath("//div[text() = 'Drag me around']"));
+//        act.dragAndDropBy(dragmearound, 100, 100);
+//
+//        WebElement dragAndDropElement = driver.findElement(By.xpath("//div[text() = 'Drag me to target']"));
+//        WebElement destination = driver.findElement(By.xpath("//div[@class = 'box drop-target react-draggable']"));
+//
+//        act.dragAndDrop(dragAndDropElement, destination).build().perform();
     }
 }
