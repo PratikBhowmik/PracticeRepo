b0893cbc75ef3de730ad3f172caed0c3faf594d3
Pratik <impratikbhowmik@gmail.com>
2022-11-24 07:45:03 +0530

    just added some practice code

--- app/src/main/java/dsa/ClassRep.java
+++ app/src/main/java/dsa/ClassRep.java
@@ -1,15 +1,41 @@
 package dsa;
+
 import java.util.Scanner;
+
 public class ClassRep {
-    static String classRep(int[] num, int n) {
-        for (int index = 0; index < num.length; index++) {
-            if (index % 2 == 0 && num[index] < num[index + 2]) {
-                return "increasing";
-            } else if (index % 2 == 0 && num[index] > num[index + 2]) {
-                return "decreasing";
+    static String classRepresentative(int[] num, int n) {
+//        for (int index = 0; index < num.length; index += 2) {
+//            if (num[index] < num[index + 2]) {
+//                return "increasing";
+//            } else if (num[index] > num[index + 2]) {
+//                return "decreasing";
+//            }
+//        }
+//        return "none";
+
+        if (increasing(num)) {
+            return "increasing";
+        } else if (decreasing(num)) {
+            return "decreasing";
+        } else {
+            return "none";
+        }
+    }
+
+    public static boolean increasing(int[] num) {
+        for (int i = 0; i < num.length - 2; i++) {
+            if (num[i] < num[i + 2]) {
             }
         }
-        return "none";
+        return true;
+    }
+
+    public static boolean decreasing(int[] num) {
+        for (int i = 0; i < num.length - 2; i++) {
+            if (num[i] > num[i + 2]) {
+            }
+        }
+        return true;
     }
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
@@ -18,6 +44,6 @@ public class ClassRep {
         for (int i = 0; i < n; i++) {
             num[i] = scan.nextInt();
         }
-        System.out.println(classRep(num, n));
+        System.out.println(classRepresentative(num, n));
     }
 }
---
+++ app/src/main/java/dsa/LongestCommonPrefix.java
@@ -0,0 +1,40 @@
+package dsa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LongestCommonPrefix {
+    static int minLength(String[] strs) {
+        int min = strs[0].length();
+        for (int i = 1; i < strs.length; i++) {
+            if (strs[i].length() < min) {
+                min = strs[i].length();
+            }
+        }
+        return min;
+    }
+
+    public String longestCommonPrefix(String[] strs) {
+        int minlength = minLength(strs);
+        String result = "";
+        char current;
+        for (int i = 0; i < minlength; i++) {
+            current = strs[0].charAt(i);
+            for (int j = 0; j < strs.length; j++) {
+                if (strs[j].charAt(i) != current) {
+                    return result;
+                }
+            }
+            result += current;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String[] strings = in.readLine().split(" ");
+        String prefixString = new LongestCommonPrefix().longestCommonPrefix(strings);
+        System.out.print(prefixString);
+    }
+}
