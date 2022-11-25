aff5137f1166fb526822a95b92d391536fa9ac6d
Pratik <impratikbhowmik@gmail.com>
2022-11-25 09:34:14 +0530

    added code

---
+++ app/src/main/java/corejavatwo/JustCheck.java
@@ -0,0 +1,9 @@
+package corejavatwo;
+
+public class JustCheck {
+    public static void main(String[] args) {
+        int n = 9;
+        int res = n/2;
+        System.out.println(res);
+    }
+}
--- app/src/main/java/dsa/MatrixTsum.java
+++ app/src/main/java/dsa/MatrixTsum.java
@@ -6,20 +6,21 @@ import java.util.Scanner;
 
 public class MatrixTsum {
     static int matrixTSum(int n, int[][] matrix) {
+
         int rowSum = 0;
         int colSum = 0;
-        int totalSum = 0;
-        for (int i = 0; i < matrix[n].length; i++) {
-            for (int j = 0; j < matrix[n].length; j++) {
-
-            }
+        for(int i = 0; i < n; i++){
+            rowSum += matrix[0][i];
         }
-        return totalSum;
+        for (int i = 1; i < n; i++){
+            colSum += matrix[i][n/2];
+        }
+        return rowSum+colSum;
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter row and column they will be always equal");
+        System.out.println("Enter no of rows and columns you want");
         int n = sc.nextInt();
         System.out.println("Enter data for the matrix");
         int matrix[][] = new int[n][n];
