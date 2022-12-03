ab463e083d5b01059488e1482811ef3dcafa38c7
Pratik <impratikbhowmik@gmail.com>
2022-12-03 22:45:12 +0530

    committed dsa problems

---
+++ app/src/main/java/dsa/Palindrome.java
@@ -0,0 +1,43 @@
+package dsa;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Palindrome {
+    public String isPalindrome(String s) {
+
+
+        String trimmed = s.replaceAll("[^A-Za-z0-9]", "");
+
+
+        String reversed = "";
+
+        int len = trimmed.length();
+
+        char[] allChars = trimmed.toCharArray();
+
+
+        for (int i = len - 1; i >= 0; i--) {
+
+            reversed += allChars[i];
+
+        }
+
+
+        if (trimmed.equalsIgnoreCase(reversed)) {
+            return "true";
+        } else {
+            return "false";
+        }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String line = in.readLine();
+        String result = new Palindrome().isPalindrome(line);
+        System.out.print(result);
+    }
+}
