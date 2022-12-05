a4598ba3b605c1ad3d19fc811e430254fa567820
Pratik <impratikbhowmik@gmail.com>
2022-12-05 19:40:49 +0530

    Added interface

---
+++ app/src/main/java/corejavatwo/ExcelOperations.java
@@ -0,0 +1,18 @@
+package corejavatwo;
+
+public class ExcelOperations implements operationOnTestData {
+    @Override
+    public void readData(String filename) {
+
+    }
+
+    @Override
+    public String writeData() {
+        return null;
+    }
+
+    @Override
+    public void updateData(String filename, String result) {
+
+    }
+}
---
+++ app/src/main/java/corejavatwo/operationOnTestData.java
@@ -0,0 +1,10 @@
+package corejavatwo;
+
+public interface operationOnTestData {
+
+    void readData(String filename);
+
+    String writeData();
+
+    void updateData(String filename, String result);
+}
