ba35b82e327cef7b0e7dbe17c45b072ba77c46fd
Pratik <impratikbhowmik@gmail.com>
2023-05-20 21:51:35 +0530

    Changes

--- app/src/main/java/problemSolving/DiamondPrinting.java
+++ app/src/main/java/problemSolving/DiamondPrinting.java
@@ -1,30 +1,46 @@
 package problemSolving;
+
 import java.util.Scanner;
-public class DiamondPrinting {
+
+class DiamondPrinting {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String[][] pattern = diamondPrinting(n);
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < i; i++) {
-                System.out.println(pattern[i][j]);
-            }
-        }
+        String[] pattern = diamondPrinting(n);
+        for (int i = 0; i < 2 * n - 1; i++)
+            System.out.println(pattern[i]);
     }
-    static String[][] diamondPrinting(int n) {
-        String [][] str = new String[n][n - 1];
+
+    static String[] diamondPrinting(int n) {
+        String[] pattern = new String[2 * n - 1];
+        int stars = 1;
+        int spaces = n - 1;
+
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                str[i][j] = "*";
-            }
-            System.out.println();
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < stars; j++)
+                sb.append("*");
+            for (int j = 0; j < spaces; j++)
+                sb.append(" ");
+            pattern[i] = sb.toString();
+            stars++;
+            spaces--;
         }
-        for (int k = 0; k < n-1; k++ ) {
-            for (int l = n-1; l > k; l--){
-                str[l][k] = "*";
-            }
+
+        stars = n - 1;
+        spaces = 1;
+
+        for (int i = n; i < 2 * n - 1; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j < stars; j++)
+                sb.append("*");
+            for (int j = 0; j < spaces; j++)
+                sb.append(" ");
+            pattern[i] = sb.toString();
+            spaces++;
+            stars--;
         }
-        return str;
+
+        return pattern;
     }
 }
-
